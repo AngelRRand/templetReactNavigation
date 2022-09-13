1d5f0b94090be40cfc91679b40fa0fac79e3175f
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-13 13:21:29 -0300

    styles

---
+++ src/Views/Nosotros.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View, StatusBar} from 'react-native';
+
+const Nosotros = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+export default Nosotros;
